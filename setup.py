7d8b491a53cd5f24ef3be051a7a7e0b236926201
watchakorn-18k <porton2559@gmail.com>
2023-05-03 05:46:02 +0700

    First Update 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from ppy import __app_name__, __version__
 
 setup_args = dict(
-    name=__app_name__,
+    name="ppy",
     version=__version__,
     description="PPython Basic for Teach",
     author="wk18k",
